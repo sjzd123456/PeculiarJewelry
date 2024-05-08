6a9375bd4a4b4ff4377739f1f95665233066d144
GabeHasWon <gabriel@guevarameyer.com>
2024-05-07 19:28:04 -0500

    0.1.0.8

--- Content/JewelryMechanic/Globals/JewelGlobalItem.cs
+++ Content/JewelryMechanic/Globals/JewelGlobalItem.cs
@@ -5,16 +5,13 @@ namespace PeculiarJewelry.Content.JewelryMechanic.Globals;
 
 internal class JewelGlobalItem : GlobalItem
 {
-    private static int[] GemOres = System.Array.Empty<int>();
+    private static int[] GemOres = [];
 
-    public override void Load()
-    {
-        GemOres = new int[] { TileID.Ruby, TileID.Amethyst, TileID.Emerald, TileID.Ruby, TileID.Diamond, TileID.Topaz, TileID.Sapphire };
-    }
+    public override void Load() => GemOres = [TileID.Ruby, TileID.Amethyst, TileID.Emerald, TileID.Ruby, TileID.Diamond, TileID.Topaz, TileID.Sapphire];
 
     public override void OnSpawn(Item item, IEntitySource source)
     {
-        if (source is EntitySource_TileBreak tileBreak && GemOres.Contains(Main.tile[tileBreak.TileCoords].TileType))
+        if (source is EntitySource_TileBreak tile && WorldGen.InWorld(tile.TileCoords.X, tile.TileCoords.Y) && GemOres.Contains(Main.tile[tile.TileCoords].TileType))
         {
             if (Main.rand.NextBool(60))
             {
